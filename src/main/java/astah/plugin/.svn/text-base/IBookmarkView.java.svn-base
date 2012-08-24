64b449472493dc0d12be427a053c0b59bb70a380
Kenji Fujimoto <kemo_zu@hotmail.com>
2012-08-24 10:49:49 +0900

    初コミット

---
+++ src/main/java/astah/plugin/.svn/text-base/IBookmarkView.java.svn-base
@@ -0,0 +1,6 @@
+package astah.plugin;
+
+public interface IBookmarkView {
+    public void update();
+    public void update(BookmarkModel selectedMode, boolean isEditing);
+}
---
+++ src/test/java/astah/plugin/TemplateActionTest.java
@@ -0,0 +1,15 @@
+package astah.plugin;
+
+import org.junit.Test;
+
+
+public class TemplateActionTest {
+	
+	@Test
+	public void testname() throws Exception {
+		
+	}
+
+}
+
+
